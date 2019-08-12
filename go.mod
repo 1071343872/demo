53fd67fbf54dc9d357487fd3a2d67d91eab0df9e
yangzhenyu <1071343872@qq.com>
2019-08-12 12:19:19 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/1071343872/demo
+
+go 1.12
+
+require github.com/1071343872/foo v0.0.0-20190812040500-4d80ba6587e5 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import ( 
+    "fmt"
+    "github.com/1071343872/foo"
+)
+
+func main(){
+    fmt.Println(foo.Greet("GoModule"))    
+}
