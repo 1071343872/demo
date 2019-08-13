72402734062e71a8898889abac6e06dbe64db5e8
yangzhenyu <1071343872@qq.com>
2019-08-13 12:10:23 +0800

    demo commit

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/1071343872/demo
 
 go 1.12
 
-require github.com/1071343872/foo v0.0.0-20190812040500-4d80ba6587e5 // indirect
+require (
+	github.com/1071343872/foo v0.0.0-20190812063631-240da294c777
+	github.com/1071343872/test v0.0.0-20190813020631-5146150de7ee
+	github.com/cihub/seelog v0.0.0-20151216151435-d2c6e5aa9fbf
+	github.com/codegangsta/cli v1.20.0
+	github.com/davyxu/tabtoy v0.0.0-20190715035631-8c93fb6a7d8e
+	gitlab.taiyouxi.cn/platform/planx v0.0.0-20171124085011-572d1f9be0eb
+)
--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
-import ( 
-    "fmt"
-    "github.com/1071343872/foo"
+import (
+	"fmt"
+	"github.com/1071343872/demo/test" //本工程的包引入
+	"github.com/1071343872/foo"  //第三方的go modules包引入
+	"github.com/1071343872/test/hello" //第三方非go modules包引入
+	_ "github.com/cihub/seelog"
+	_ "github.com/codegangsta/cli"
+	_ "github.com/davyxu/tabtoy/util"
+	"gitlab.taiyouxi.cn/platform/planx/version"  //gitlab私库
 )
 
-func main(){
-    fmt.Println(foo.Greet("GoModule"))    
+func main() {
+	fmt.Println(foo.Greet("GoModule"))
+	test.Hh()
+
+	//logs.Debug("test")
+	hello.SayHello()
+
+	fmt.Println(version.GetVersion())
 }
